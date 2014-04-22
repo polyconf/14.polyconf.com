5bc87ebdda1aec0a2ddde780ddefe8a1a3b028c5
Zaiste! <oh@zaiste.net>
2014-04-22 13:20:24 +0200

    improv: final layout

---
+++ js/vendor/true.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+   var logoLines = $("#logo .line span");
+
+   logoLines
+   	.attr("data-anchor-target","#top")
+   	.attr("data-0","height:3px")
+   	.attr("data-100-top-bottom","height:2px");
+   	
+
+//   logoLines.css({ width: "0%" });
+//   
+//   var lineSpeed = 1000;
+//   
+//   $("#line1 span,#line2 span").transition({ width: "100%", delay: 2000 }, lineSpeed, function() {
+//   
+//       $("#line3 span, #line4 span, #line5 span, #line6 span").transition({ width: "100%" }, lineSpeed, function() {
+//       
+//         $("#line7 span, #line8 span, #line9 span").transition({ width: "100%" }, lineSpeed, function() {
+//         
+//           $("#about").transition({opacity: 1,delay: 1000}, function() {
+//           
+//             $(".city, .date").transition({opacity: 1,delay: 1000})
+//                       
+//           })
+//         
+//         
+//         
+//         })
+//       
+//       } )
+//   
+//   });
+// 
+//   var letters = $("#name .letter");
+// 
+//   letters.css({ opacity: 0 });
+// 
+//   var chain = function(toAnimate, ix){
+//       if(toAnimate[ix]){
+//           if (ix==0) delayLetters=1000; else delayLetters=0;
+//           $(toAnimate[ix]).transition({opacity:1, delay:delayLetters}, 400, function(){ chain(toAnimate, ix + 1 )});
+//       }
+//   };
+//   
+//   chain(letters,0);
+
+
+  var s = skrollr.init();
+
+
+})
